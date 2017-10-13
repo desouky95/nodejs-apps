ca4985b0758b06ac6185cea8bcd7a94f27c1cda7
Ismail Moustafa <tubesniper@gmail.com>
2017-10-14 00:15:17 +0200

    add .

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 // make express look in the public directory for assets (css/js/img)
@@ -16,7 +17,7 @@ app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
     // res.render('index');
-    res.write()
+    res.render('index.html');
 });
 
 app.listen(port, function() {
